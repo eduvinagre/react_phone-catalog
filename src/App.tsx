0cfaf92803edab07fb2a0a7053777475c933b6be
HigorPedroso <higor533@gmail.com>
2024-06-04 15:03:03 -0300

    Page grid and adaptive layout (without card)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import './assets/scss/variables.scss';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { DefaultLayout } from './layouts/default.layout';
 import { HomePage } from './pages/HomePage';
 import { PageNotFound } from './pages/PageNotFound';
+import PhonesPage from './pages/PhonesPage/PhonesPage';
 
 export const App = () => (
   <Router>
@@ -9,6 +11,7 @@ export const App = () => (
       <Route path="/" element={<DefaultLayout />}>
         <Route index element={<HomePage />} />
         <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="phonespage" element={<PhonesPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
