ba731b75d7adb880356203e3b65452c2348a7b1c
eduvinagre <eduvinagre@gmail.com>
2024-06-04 00:24:27 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import './App.scss';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { DefaultLayout } from './layouts/default.layout';
+import { PeoplePage } from './pages/PeoplePage';
+import { HomePage } from './pages/HomePage';
+import { PageNotFound } from './pages/PageNotFound';
 
 export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="people/:selectedPerson?" element={<PeoplePage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  </Router>
 );
