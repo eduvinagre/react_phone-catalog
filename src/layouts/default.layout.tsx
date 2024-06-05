674bbc00969983126c14fa2346e0b21d11aad0cb
eduvinagre <eduvinagre@gmail.com>
2024-06-05 20:35:04 -0300

    fix: folders first letter to uppercase

--- src/layouts/default.layout.tsx
+++ src/layouts/default.layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
 import '../App.css';
-import { Header } from '../components/header';
+import { Header } from '../components/Header';
 // import { Navbar } from '../components/Navbar'; COMPONENT SUGGESTION
 
 export const DefaultLayout = () => {
