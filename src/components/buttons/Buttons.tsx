643f3f0dedb58af7e1d3d868b85008d7c9d4b805
eduvinagre <eduvinagre@gmail.com>
2024-06-06 17:46:13 -0300

    first commit

---
+++ src/components/buttons/Buttons.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useAppContext } from '../../context/AppContext';
+import { Product } from '../../types/Product';
+import styles from 'Buttons.scss';
+import { BASE_URL } from '../../utils/const';
+
+type Props = {
+  product: Product;
+};
+
+export const ActionButtons: React.FC<Props> = ({ product }) => {
+  const { addToFavorites, removeFromFavorites, favorites, cart, addToCart, removeFromCart } =
+    useAppContext();
+  const { theme } = useTheme();
+
+  const isFavorite = favorites.some(favProduct => favProduct.id === product.id);
+
+  const handleFavoriteClick = () => {
+    if (isFavorite) {
+      removeFromFavorites(product.id);
+    } else {
+      addToFavorites(product);
+    }
+  };
+
+  const isProductInCart = cart.some(cartItem => cartItem.product.id === product.id);
+
+  const handleCartClick = () => {
+    if (isProductInCart) {
+      removeFromCart(product.id);
+    } else {
+      addToCart(product);
+    }
+  };
+
+  const favoritesIconSrc = () => {
+    if (isFavorite) {
+      return `${BASE_URL}/img/icons/SelectedToFaforitesIcon.svg`;
+    }
+
+    return getFavoritesIconSrc(theme);
+  };
+
+  return (
+    <div className={styles.buttons}>
+      <button className={styles.buttonCard} onClick={handleCartClick}>
+        <p className={styles.buttonText}>{isProductInCart ? 'Remove' : 'Add to cart'}</p>
+      </button>
+      <button className={styles.buttonFavorite} onClick={handleFavoriteClick}>
+        <img className={styles.buttonFavoriteIcon} src={favoritesIconSrc()} alt="favorite" />
+      </button>
+    </div>
+  );
+};
