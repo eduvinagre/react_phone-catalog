59fc871e231e1cd603348386044a219739f1df8e
HigorPedroso <higor533@gmail.com>
2024-06-06 09:24:20 -0300

    fix conflits and improves

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './assets/scss/variables.scss';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { DefaultLayout } from './layouts/default.layout';
+import { HomePage } from './pages/HomePage';
+import PhonesPage from './pages/PhonesPage';
+import { CartPage } from './pages/cart-page';
+import { PageNotFound } from './pages/pageNotFound/PageNotFound';
+
+export const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="phonespage" element={<PhonesPage />} />
+        <Route path='cart' element={<CartPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  </Router>
+);
